89202e7c9a89346d42d115fccae5100a592c916a
Gabriel-R-Parra <77903365+Gabriel-R-Parra@users.noreply.github.com>
2024-08-20 14:34:32 -0600

    Fixed run script

--- run.bash
+++ run.bash
@@ -1,11 +1,7 @@
 if [ ! "$(docker ps -q -f name=jove)" ]; then
     if [ ! "$(docker ps -aq -f name=jove)" ]; then
-        # If the container does not exist, run it
-        docker run -d -p 8888:8888 --name jove jove-environment
+        docker run -d -p 8888:8888 --name jove -v "./Jove:/opt/Jove" jove-environment
     else
-        # If the container exists but is stopped, start it
         docker start jove
     fi
 fi
-
-docker exec -it my_running_container /bin/bash
